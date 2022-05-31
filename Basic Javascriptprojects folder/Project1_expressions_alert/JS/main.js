1b0e5e7e9a987988e298e3dd5156c86a766c5451
Gary Marc Killen <47016687+killengary37@users.noreply.github.com>
2022-05-30 22:47:27 -0700

    Add files via upload

---
+++ Basic Javascriptprojects folder/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+
+var Family = "The arezzinis", Dad = "Jeremiah", Mom = "Hermione", //defining variables family and the individual members
+    daughter = "Penny", Son = "Zorro";
+
+document.write(Dad);    // calling the variable Dad 
+
+window.alert("Gary is awesome"); //using the window alert method 
+
+document.write(12+12+12);  //using an expression to add three sets of numbers together
+
+let text1 = "Have"; //creating string for variable text1, text2, and text3
+let text2 = "a"; 
+let text3 = "great day!!!";
+
+
+document.write(text1 + text2 + text3); //placing all three string variable into and expression to create a full sentence
+
+
+
+
