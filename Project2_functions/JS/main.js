0b1d58e533fbe0ec83976e3a2245e3d0b19d3bdf
Gary Marc Killen <47016687+killengary37@users.noreply.github.com>
2022-05-31 11:40:39 -0700

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction(){   //defining a function and naming it 
+    var sentence = "I am learning"; //defining a variable and naming it 
+    sentence += "a lot from this book"; // using a variable to concatenate a string
+    document.getElementById("Concatenate").innerHTML = sentence; //Putting the value of the variable into the Html ElementFromPoint with the Concatenate id
+}
